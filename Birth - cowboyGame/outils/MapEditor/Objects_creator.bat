cbc80792a6e1d087ef7766da4db6f366a8a4d55d
galinettecendree <53626355+galinettecendree@users.noreply.github.com>
2019-11-11 21:27:30 +0100

    changement de nom de dossier

---
+++ Birth - cowboyGame/outils/MapEditor/Objects_creator.bat
@@ -0,0 +1,2 @@
+cd C:\Users\fredj\Documents\Processing\Projects\MapEditor\
+start  C:/Users/fredj/Documents/Processing/Projects/MapEditor/tools/Objects_creator/objects_creator.exe
--- Birth - cowboyGame/outils/levelCreator - uncompiled/Objects_creator.bat
+++
@@ -1,2 +0,0 @@
-cd C:\Users\fredj\Documents\Processing\Projects\MapEditor\
-start  C:/Users/fredj/Documents/Processing/Projects/MapEditor/tools/Objects_creator/objects_creator.exe
